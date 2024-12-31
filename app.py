001ba9ff8db83a101343a4d6ec457b09d9746b78
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2024-12-31 11:06:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return jsonify(message="You are welcome!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
