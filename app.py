28fd148748c18553171edf189a6dec2e8159abaa
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2024-12-31 17:45:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def welcome():
     return jsonify(message="You are welcome!")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
