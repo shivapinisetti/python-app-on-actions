434100ee5e3e5b39c5b8c44d0f85b738186e136e
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-01-01 11:22:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def welcome():
-    return jsonify(message="Welcome to Code Kamikaze, And Subscribe to Code Kamikaze")
+    return jsonify(message="You are welcome!")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False)
